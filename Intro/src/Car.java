0953a7da833a6dddb9152b0a3455a2794f9662b1
Miss Rizwana Saife <saifriz_19@yahoo.co.in>
2023-04-02 19:00:39 +0530

    commit

--- Intro/src/Car.java
+++ Intro/src/Car.java
@@ -1,16 +1,24 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Car {
 
 	public static void main(String[] args) {
 		
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Miss Rizwana Saife\\Desktop\\Testing\\Selenium-jars\\drivers\\chromedriver.exe");
+
+		/*
+		 * ChromeOptions options = new ChromeOptions();
+		 * options.addArguments("--remote-allow-origins=*"); ChromeDriver driver = new
+		 * ChromeDriver(options);
+		 */
+		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.manage().window().maximize();
 		
-		driver.get("https://www.google.co.in");
+		driver.get("https://www.automationtesting.co.uk/buttons.html");
 	}
 
 }
---
+++ Intro/src/section11/testnginstall.java
@@ -0,0 +1,17 @@
+package section11;
+
+import org.testng.annotations.Test;
+
+public class testnginstall {
+
+	@Test
+	public void helloWorld() {
+		System.out.println("Hello World!");
+	}
+	
+	@Test
+	public void helloWorld2() {
+		System.out.println("Hello World!");
+	}
+	
+}
